dd797ea2658cd3af958584c1fe20aab12eaa370a
tanquan <2685303161@qq.com>
2023-07-30 21:37:58 +0800

    add 搜索插件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,15 +1,23 @@
 import { defineUserConfig, defaultTheme } from "vuepress";
 import sidebar from "./sidebar";
 import navbar from "./navbar";
+import searchPlugin from "@vuepress/plugin-search";
 
 export default defineUserConfig({
   lang: "zh-CN",
   title: "木佑编程文档",
   description: "这是我的第一个 VuePress 站点",
-  base: "/",
+  base: "/program-docs/",
+  dest: './public',
   theme: defaultTheme({
     logo: 'logo.jpg',
     sidebar,
     navbar,
   }),
+  head:[['link', { rel: 'icon', href: '/logo.jpg' }]],
+  plugins: [
+    searchPlugin({
+      // 配置项
+    }),
+  ]
 });
